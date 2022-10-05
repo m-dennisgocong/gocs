fda2d70191e129beb874ed516adc64ec614a3b10
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-05 15:22:43 +0700

    add open/close nav menu for small screen size

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,22 +1,31 @@
+import { useState } from 'react';
 import close from './../assets/close.svg'
 import menu from './../assets/menu.svg'
+
 const Navigation = () => {
+
+    const handleNavMenu = () => {
+        setNavMenu(!navMenu)
+    };
+
+    const [navMenu, setNavMenu] = useState(false);
 
     return(
         <header id="Navigation">
         
             <a href="#"><div className='nickname'>Goc<span>s</span></div></a>
 
-            <nav>
+            <nav style={navMenu ? {transform: "translateX(0)"} : null }>
+                <img src={close} className="close" alt="close menu" onClick={handleNavMenu}/>
                 <ul>
+                    <li><a href="#">Home</a></li>
                     <li><a href="#">Project</a></li>
                     <li><a href="#">Art</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
-                <img src={close} className="close" alt="close menu" />
             </nav>
         
-            <img src={menu} className="menu" alt="menu svg" />
+            <img src={menu} className="menu" alt="menu svg" onClick={handleNavMenu}/>
                
         </header>
     );
