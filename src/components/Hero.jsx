8d62f009e5d88d73093d35191efb7e25740885e1
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-08 10:04:51 +0700

    add hero section component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,12 @@
+import arrowDown from './../assets/arrow-down.svg'
+const Hero = () => {
+
+    return(
+        <section className="Hero">
+        <h1><span id="underline">Hi, I'm Dennis Goc-ong</span></h1>
+        <p><span id="rule">Frontend Developer</span> <br/> being able to translate any design into code makes me happy.</p>
+        <img id="arrowDown" src={arrowDown} alt="arrow point down" />
+        </section>
+    );
+}
+export default Hero;
