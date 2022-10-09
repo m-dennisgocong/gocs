ebc4963f3e227619ca00141c165f3bb2d7132e93
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-09 16:40:11 +0700

    change section class to id

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import arrowDown from './../assets/arrow-down.svg'
 const Hero = () => {
 
     return(
-        <section className="Hero">
+        <section id="Hero">
         <h1><span id="underline">Hi, I'm Dennis Goc-ong</span></h1>
         <p><span id="rule">Frontend Developer</span> <br/> being able to translate any design into code makes me happy.</p>
         <img id="arrowDown" src={arrowDown} alt="arrow point down" />
