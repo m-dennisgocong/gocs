43abae7e050ea20204a8e191132ffc45ce408bbf
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-17 18:05:34 +0700

    add Arts component for my CSS Arts collections

---
+++ src/components/Arts.jsx
@@ -0,0 +1,65 @@
+const Arts = () => {
+    return(
+    <section id="Arts">
+        <h3>CSS Arts</h3>
+        <div className="scrollong-wrapper">
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>   
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+
+            <a href="" className="card">
+                <img src="" alt="" />
+                <p>Art</p>    
+            </a>  
+            
+        </div>
+    </section>
+    );
+}
+export default Arts;
