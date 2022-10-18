bb89f9bff18fb93fa0016f9b51c7eb411920a662
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-18 14:44:00 +0700

    update: add  images source and remove <p> tag

--- src/components/Arts.jsx
+++ src/components/Arts.jsx
@@ -2,63 +2,15 @@ const Arts = () => {
     return(
     <section id="Arts">
         <h3>CSS Arts</h3>
-        <div className="scrollong-wrapper">
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>   
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-
-            <a href="" className="card">
-                <img src="" alt="" />
-                <p>Art</p>    
-            </a>  
-            
-        </div>
+        <div className="scrollong-wrapper">   
+            <a href="" className="card"><img src="/cat.png" alt="" /></a>  
+            <a href="" className="card"><img src="/twitter-logo.png" alt="" /></a>  
+            <a href="" className="card"><img src="/one-div-avocado.png" alt="" /></a>  
+            <a href="" className="card"><img src="/loading-animation.png" alt="" /></a>  
+            <a href="" className="card"><img src="/pixelated-sunglass.png" alt="" /></a>  
+            <a href="" className="card"><img src="/walter-white-collection.png" alt="" /></a>  
+            <a href="" className="card"><img src="/codepen-challenges.png" alt="" /></a>
+            <a href="" className="card"><img src="/batman-logo.png" alt="" /></a></div>
     </section>
     );
 }
