38e357d29643e0e9bf17b08904fa3b90fb83b300
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-19 12:39:24 +0700

    update: add (new src images & section description)

--- src/components/Arts.jsx
+++ src/components/Arts.jsx
@@ -2,15 +2,24 @@ const Arts = () => {
     return(
     <section id="Arts">
         <h3>CSS Arts</h3>
-        <div className="scrollong-wrapper">   
-            <a href="" className="card"><img src="/cat.png" alt="" /></a>  
-            <a href="" className="card"><img src="/twitter-logo.png" alt="" /></a>  
-            <a href="" className="card"><img src="/one-div-avocado.png" alt="" /></a>  
-            <a href="" className="card"><img src="/loading-animation.png" alt="" /></a>  
+        <p>In my spare time, I'm on <a href="https://codepen.io/dennisgocong">CodePen</a> making art out of code or doing the <a href="https://codepen.io/challenges">CodePen Challenges</a>. <br />Here's my collection, check it out:</p>
+        <div className="scrollong-wrapper snaps-inline">   
+            <a href="" className="card"><img src="/cat.png" alt="" /></a>    
+            <a href="" className="card"><img src="/avocado.png" alt="" /></a>  
+            <a href="" className="card"><img src="/twitter-logo.png" alt="" /></a> 
             <a href="" className="card"><img src="/pixelated-sunglass.png" alt="" /></a>  
-            <a href="" className="card"><img src="/walter-white-collection.png" alt="" /></a>  
-            <a href="" className="card"><img src="/codepen-challenges.png" alt="" /></a>
-            <a href="" className="card"><img src="/batman-logo.png" alt="" /></a></div>
+            <a href="" className="card"><img src="/add-even-pattern.png" alt="" /></a>  
+            <a href="" className="card"><img src="/glitch-effect.png" alt="" /></a>  
+            <a href="" className="card"><img src="/pi.png" alt="" /></a>  
+            <a href="" className="card"><img src="/counter.png" alt="" /></a>    
+            <a href="" className="card"><img src="/loading-animation.png" alt="" /></a>
+            <a href="" className="card"><img src="/batman-logo.png" alt="" /></a> 
+            <a href="" className="card"><img src="/walter-white-01.png" alt="" /></a>
+            <a href="" className="card"><img src="/walter-white-02.png" alt="" /></a>
+            <a href="" className="card"><img src="/walter-white-03.png" alt="" /></a>
+            <a href="" className="card"><img src="/walter-white-04.png" alt="" /></a>
+        
+        </div>
     </section>
     );
 }
