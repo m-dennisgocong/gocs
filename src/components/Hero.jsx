c95cbb69485942e3157352dba1457216167161f5
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-20 13:00:40 +0700

    add emoji

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = () => {
     return(
         <section id="Hero">
         <h1><span id="underline">Hi, I'm Dennis Goc-ong</span></h1>
-        <p><span id="rule">Frontend Developer</span> <br/> being able to translate any design into code makes me happy.</p>
+        <p><span id="rule">Frontend Developer</span> <br/> being able to translate any design into code makes my {'♥️🔥'}.</p>
         <img id="arrowDown" src={arrowDown} alt="arrow point down" />
         </section>
     );
