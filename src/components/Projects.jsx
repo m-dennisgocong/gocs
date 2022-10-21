c1db7e966511a6783b367a62905808c83ecab71c
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-21 13:00:53 +0700

    update: Project Section
    Import a link icons from  react-icons/fa
    
    add some padding to the projects card to align the break paragraph from the rest of the project cards also set the position to relative
    
    regroup html: put tech-stack inside project-detail div and set the postion to absolute
    
    style the links buttons and use the imported icons also set the position to absolute

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 const Projects = () => {
 
     const [view, setView] = useState(false)
@@ -9,74 +11,74 @@ const Projects = () => {
             <div id="recent-projects">
                 <h2>Recent Projects</h2>  
                 <div className="project-container">
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
                         <div className="project-detail">
                             <h3 title="">virtual-drum</h3>
                             <p className="description">Online Virtual Drum </p>
-                        </div>
                             <ul className="tech-stack">
                                 <li>HTML</li>
                                 <li>CSS</li>
                                 <li>ReactJS</li>
                             </ul>
-
-                            <div className="links">
-                                <div className="demo-link">Demo</div>
-                                <div className="source-link">Source</div>
-                            </div>
+                        </div>
+                        <div className="links">
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
+                        </div>
                         
                     </div>
 
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
 
                         <div className="project-detail">
                             <h3 title="">md-editor</h3>
                             <p className="description">markdown editor with a live preview</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>ReactJS</li>
+                                <li>API</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>ReactJS</li>
-                            <li>API</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                     </div>
 
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
                         <div className="project-detail">
                             <h3 title="">make-border-radius</h3>
                             <p className="description">CSS generator tool for border-radius, fast and easy to use.</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                         
                     </div>
 
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
 
                         <div className="project-detail">
                             <h3 title="">random-quote-machine</h3>
                             <p className="description">Random Quote Generator, share to twitter</p>
+                        
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>ReactJS</li>
+                                <li>API</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>ReactJS</li>
-                            <li>API</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                 
                     </div>
@@ -94,83 +96,88 @@ const Projects = () => {
                 <p>This is my projects for <a href="https://www.freecodecamp.org/learn/responsive-web-design/#responsive-web-design-projects">Responsive Web Design</a> course on freeCodeCamp.
                 I was able to earn a <a href="https://www.freecodecamp.org/certification/gocs/responsive-web-design">Certification</a> for completing the following: </p> 
                 <div className="project-container">
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
                         <div className="project-detail">
                             <h3 title="">Technical Documentation Page</h3>
+                            <p className="description">JavaScript technical documentation page</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                     </div>
 
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
 
                         <div className="project-detail">
                             <h3 title="">Personal Portfolio Webpage</h3>
+                            <p className="description">My first personal portfolio webpage</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                         
                     </div>
 
-                    <div className="project-tile">
-                        <div className="project-detail">
-                            <h3 title="">Survey Form</h3>
-                        </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                        </ul>
-                        <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
-                        </div>
-                    
-                    </div>
-
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
 
                         <div className="project-detail">
-                            <h3 title="">Product Landing Page</h3>
+                            <h3 title="">Product Landing Page Landing</h3>
+                            <p className="description">Created my own product landing page for Victoria's Secret</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                 
                     </div>
 
-                    <div className="project-tile">
+                    <div className="project-tile anchored large">
+                        <div className="project-detail">
+                            <h3 title="">Survey Form</h3>
+                            <p className="description">A fitness suite survey form</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                            </ul>
+                        </div>
+                        <div className="links">
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
+                        </div>
+                    
+                    </div>
+
+                    <div className="project-tile anchored large">
 
                         <div className="project-detail">
                             <h3 title="">Tribute Page</h3>
+                            <p>A tribute page for Dr. Norman Borlaug</p>
+                            <ul className="tech-stack">
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>JavaScript</li>
+                            </ul>
                         </div>
-                        <ul className="tech-stack">
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                        </ul>
                         <div className="links">
-                            <div className="demo-link">Demo</div>
-                            <div className="source-link">Source</div>
+                            <a href="" className="demo-link">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                            <a href="" className="source-link">Source <FaExternalLinkAlt className="link-icons"/></a>
                         </div>
                     
                     </div>
