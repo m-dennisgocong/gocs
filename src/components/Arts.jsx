a4476936f8467743c4b16faa771ac2009607fbf4
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-22 13:15:32 +0700

    update: h3 to h2

--- src/components/Arts.jsx
+++ src/components/Arts.jsx
@@ -1,7 +1,7 @@
 const Arts = () => {
     return(
     <section id="Arts">
-        <h3>CSS Arts</h3>
+        <h2>CSS Arts</h2>
         <p>In my spare time, I'm on <a href="https://codepen.io/dennisgocong">CodePen</a> making art out of code or doing the <a href="https://codepen.io/challenges">CodePen Challenges</a>. <br />Here's my collection, check it out:</p>
         <div className="scrollong-wrapper snaps-inline">   
             <a href="" className="card"><img src="/cat.png" alt="" /></a>    
