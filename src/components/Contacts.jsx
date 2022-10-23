01d7518ab95128e81a3c1257c57c98b2c3cbe771
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-23 11:31:56 +0700

    update: set socials href links

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,4 +1,4 @@
-import { FaGithub, FaTwitter, FaCodepen } from 'react-icons/fa';
+import { FaGithub, FaTwitter, FaCodepen, FaFreeCodeCamp } from 'react-icons/fa';
 const Contacts = () => {
 
     return(
@@ -7,7 +7,7 @@ const Contacts = () => {
             <p>Get in touch for opportunities or just to say hi! 👋 <br /> <span>m.dennisgocong@gmail.com</span></p>
 
             <div id="socials-wrapper">
-                <FaGithub/> <FaTwitter/> <FaCodepen/>
+                <a href="https://github.com/m-dennisgocong" target="_blank"><FaGithub/></a> <a href="https://twitter.com/dennisgocong" target="_blank"><FaTwitter/></a> <a href="https://www.freecodecamp.org/gocs" target="_blank"><FaFreeCodeCamp/></a> <a href="https://codepen.io/dennisgocong" target="_blank"><FaCodepen/></a>
             </div>
         </section>
     );
