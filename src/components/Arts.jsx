b9d1a07c35804f4f0c221b68199fcbde8b24f64e
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-23 11:31:25 +0700

    update: set arts href links

--- src/components/Arts.jsx
+++ src/components/Arts.jsx
@@ -2,22 +2,22 @@ const Arts = () => {
     return(
     <section id="Arts">
         <h2>CSS Arts</h2>
-        <p>In my spare time, I'm on <a href="https://codepen.io/dennisgocong">CodePen</a> making art out of code or doing the <a href="https://codepen.io/challenges">CodePen Challenges</a>. <br />Here's my collection, check it out:</p>
+        <p>In my spare time, I'm on <a href="https://codepen.io/dennisgocong" target="_blank">CodePen</a> making art out of code or doing the <a href="https://codepen.io/challenges" target="_blank">CodePen Challenges</a>. <br />Here's my collection, check it out:</p>
         <div className="scrollong-wrapper snaps-inline">   
-            <a href="" className="card"><img src="/cat.png" alt="" /></a>    
-            <a href="" className="card"><img src="/avocado.png" alt="" /></a>  
-            <a href="" className="card"><img src="/twitter-logo.png" alt="" /></a> 
-            <a href="" className="card"><img src="/pixelated-sunglass.png" alt="" /></a>  
-            <a href="" className="card"><img src="/add-even-pattern.png" alt="" /></a>  
-            <a href="" className="card"><img src="/glitch-effect.png" alt="" /></a>  
-            <a href="" className="card"><img src="/pi.png" alt="" /></a>  
-            <a href="" className="card"><img src="/counter.png" alt="" /></a>    
-            <a href="" className="card"><img src="/loading-animation.png" alt="" /></a>
-            <a href="" className="card"><img src="/batman-logo.png" alt="" /></a> 
-            <a href="" className="card"><img src="/walter-white-01.png" alt="" /></a>
-            <a href="" className="card"><img src="/walter-white-02.png" alt="" /></a>
-            <a href="" className="card"><img src="/walter-white-03.png" alt="" /></a>
-            <a href="" className="card"><img src="/walter-white-04.png" alt="" /></a>
+            <a href="https://codepen.io/dennisgocong/pen/dyJqzQX" target="_blank" className="card"><img src="/cat.png" alt="" /></a>    
+            <a href="https://codepen.io/dennisgocong/pen/zYpZXjB" target="_blank" className="card"><img src="/avocado.png" alt="" /></a>  
+            <a href="https://codepen.io/dennisgocong/pen/WNdEqpv" target="_blank" className="card"><img src="/twitter-logo.png" alt="" /></a> 
+            <a href="https://codepen.io/dennisgocong/pen/XWVadVK" target="_blank" className="card"><img src="/pixelated-sunglass.png" alt="" /></a>  
+            <a href="https://codepen.io/dennisgocong/pen/rNJXzze" target="_blank" className="card"><img src="/add-even-pattern.png" alt="" /></a>  
+            <a href="https://codepen.io/dennisgocong/pen/MWQxJKa" target="_blank" className="card"><img src="/glitch-effect.png" alt="" /></a>  
+            <a href="https://codepen.io/dennisgocong/pen/JjLjwEo" target="_blank" className="card"><img src="/pi.png" alt="" /></a>  
+            <a href="https://codepen.io/dennisgocong/pen/ExQGmEW" target="_blank" className="card"><img src="/counter.png" alt="" /></a>    
+            <a href="https://codepen.io/dennisgocong/pen/QWaggKG" target="_blank" className="card"><img src="/loading-animation.png" alt="" /></a>
+            <a href="https://codepen.io/dennisgocong/pen/vYeQyEW" target="_blank" className="card"><img src="/batman-logo.png" alt="" /></a> 
+            <a href="https://codepen.io/dennisgocong/pen/jOZoKex" target="_blank" className="card"><img src="/walter-white-01.png" alt="" /></a>
+            <a href="https://codepen.io/dennisgocong/pen/yLvWqPd" target="_blank" className="card"><img src="/walter-white-02.png" alt="" /></a>
+            <a href="https://codepen.io/dennisgocong/pen/MWQMaaL" target="_blank" className="card"><img src="/walter-white-03.png" alt="" /></a>
+            <a href="https://codepen.io/dennisgocong/pen/oNErbrJ" target="_blank" className="card"><img src="/walter-white-04.png" alt="" /></a>
         
         </div>
     </section>
