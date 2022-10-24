85725d4a79e188d64fe546a9d7e5fd687515cbf7
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-24 13:49:58 +0700

    update: add code indentation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,9 @@ const Hero = () => {
 
     return(
         <section id="Hero">
-        <h1><span id="underline">Hi, I'm Dennis Goc-ong</span></h1>
-        <p><span id="rule">Frontend Developer</span> <br/> being able to translate any design into code makes my {'♥️🔥'}.</p>
-        <a href="#Projects"><img id="arrowDown" src={arrowDown} alt="arrow point down" /></a>
+            <h1><span id="underline">Hi, I'm Dennis Goc-ong</span></h1>
+            <p><span id="rule">Frontend Developer</span> <br/> being able to translate any design into code makes my {'♥️🔥'}.</p>
+            <a href="#Projects"><img id="arrowDown" src={arrowDown} alt="arrow point down" /></a>
         </section>
     );
 }
