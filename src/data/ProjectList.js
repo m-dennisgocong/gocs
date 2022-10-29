96732d4bfd837b72772e03c0fb0b55a8a76a0ef2
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-29 15:53:30 +0700

    add list of project data

---
+++ src/data/ProjectList.js
@@ -0,0 +1,86 @@
+export const recentProject = [
+    {
+        title: "virtual-drum",
+        description: "Online Virtual Drum",
+        techstack : [ "HTML","SCSS","React"],
+        links : {
+            demo : "https://online-virtual-drum.netlify.app/",
+            source : "https://github.com/m-dennisgocong/virtual-drum"
+        }
+    },
+    {
+        title: "md-editor",
+        description: "markdown editor with a live preview",
+        techstack : ["HTML","SCSS","React"],
+        links : {
+            demo : "https://m-dennisgocong.github.io/md-editor/",
+            source : "https://github.com/m-dennisgocong/md-editor"
+        }
+    },
+    {
+        title: "make-border-radius",
+        description: "CSS generator tool for border-radius, fast and easy to use.",
+        techstack : ["HTML","CSS","JavaScript"],
+        links : {
+            demo : "https://make-border-radius.netlify.app/",
+            source : "https://github.com/m-dennisgocong/make-border-radius"
+        }
+    },
+    {
+        title: "random-quote-machine",
+        description: "Random Quote Generator, share to twitter",
+        techstack : ["HTML","CSS","React"],
+        links : {
+            demo: "https://m-dennisgocong.github.io/random-quote-machine/",
+            source: "https://github.com/m-dennisgocong/random-quote-machine"
+        }
+    }
+];
+
+export const moreProject = [
+    {
+        title: "Technical Documentation Page",
+        description: "JavaScript technical documentation page",
+        techstack : [ "HTML","CSS","JavaScript"],
+        links : {
+            demo : "https://codepen.io/dennisgocong/full/VwyogOx",
+            source : "https://codepen.io/dennisgocong/pen/VwyogOx"
+        }
+    },
+    {
+        title: "Personal Portfolio Webpage",
+        description: "My first personal portfolio webpage",
+        techstack : [ "HTML","CSS","JavaScript"],
+        links : {
+            demo : "https://codepen.io/dennisgocong/full/xxYENXR",
+            source : "https://codepen.io/dennisgocong/pen/xxYENXR"
+        }
+    },
+    {
+        title: "Product Landing Page Landing",
+        description: "Created my own product landing page for Victoria's Secret",
+        techstack : [ "HTML","CSS","JavaScript"],
+        links : {
+            demo : "https://codepen.io/dennisgocong/full/BaJEKja",
+            source : "https://codepen.io/dennisgocong/pen/BaJEKja"
+        }
+    },
+    {
+        title: "Survey Form",
+        description: "A fitness suite survey form",
+        techstack : [ "HTML","SCSS"],
+        links : {
+            demo : "https://codepen.io/dennisgocong/full/xxXNKvK",
+            source : "https://codepen.io/dennisgocong/pen/xxXNKvK"
+        }
+    },
+    {
+        title: "Tribute Page",
+        description: "A tribute page for Dr. Norman Borlaug",
+        techstack : [ "HTML","SCSS"],
+        links : {
+            demo : "https://codepen.io/dennisgocong/full/NWaLmbB",
+            source : "https://codepen.io/dennisgocong/pen/NWaLmbB"
+        }
+    }
+]; 
