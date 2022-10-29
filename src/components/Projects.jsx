8d76e59652454fd631ccc104a6272aea89356d43
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-10-29 16:05:52 +0700

    add reusable function that creates project element
    import the data from projects list
    and use projectElements function that creates the project elements instead of writing the elements manually

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
 import { FaExternalLinkAlt } from "react-icons/fa";
+import {recentProject, moreProject} from './../data/ProjectList.js';
 
 const Projects = () => {
 
     const [view, setView] = useState(false)
+
+    // create project Elements
+    const projectElements = (project) => {
+        return(
+            <div key={project.title} className="project-tile anchored large">
+                <div className="project-detail">
+                    <h3 title="">{project.title}</h3>
+                    <p className="description">{project.description}</p>
+                    <ul className="tech-stack">
+                        {project.techstack.map((tech, index)=><li key={index}>{tech}</li>)}
+                    </ul> 
+                </div>
+                <div className="links">
+                    <a href={project.links.demo} className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
+                    <a href={project.links.source} className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
+                </div>
+            </div>
+        );
+    }
 
     return(
         <section id="Projects">
@@ -11,178 +31,23 @@ const Projects = () => {
             <div id="recent-projects">
                 <h2>Recent Projects</h2>  
                 <div className="project-container">
-                    <div className="project-tile anchored large">
-                        <div className="project-detail">
-                            <h3 title="">virtual-drum</h3>
-                            <p className="description">Online Virtual Drum </p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>SCSS</li>
-                                <li>React</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://online-virtual-drum.netlify.app/" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://github.com/m-dennisgocong/virtual-drum" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                        
-                    </div>
-
-                    <div className="project-tile anchored large">
-
-                        <div className="project-detail">
-                            <h3 title="">md-editor</h3>
-                            <p className="description">markdown editor with a live preview</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>SCSS</li>
-                                <li>React</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://m-dennisgocong.github.io/md-editor/" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://github.com/m-dennisgocong/md-editor" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                    </div>
-
-                    <div className="project-tile anchored large">
-                        <div className="project-detail">
-                            <h3 title="">make-border-radius</h3>
-                            <p className="description">CSS generator tool for border-radius, fast and easy to use.</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://make-border-radius.netlify.app/" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://github.com/m-dennisgocong/make-border-radius" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                        
-                    </div>
-
-                    <div className="project-tile anchored large">
-
-                        <div className="project-detail">
-                            <h3 title="">random-quote-machine</h3>
-                            <p className="description">Random Quote Generator, share to twitter</p>
-                        
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>React</li>
-                                <li>API</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://m-dennisgocong.github.io/random-quote-machine/" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://github.com/m-dennisgocong/random-quote-machine" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                
-                    </div>
+                    {recentProject.map((project)=> projectElements(project))}
                 </div>
-
-                
             </div>
             
+            {/* View More */}
             <div style={view ? {display : 'none'} : null} id="view-more-container"><button className="more-projects" onClick={()=> setView(!view)}>View more</button></div>
 
             { view && 
-
-            <div id="FreeCodeCamp-projects">
-                <h2>FreeCodeCamp(Projects)</h2> 
-                <p>This is my projects for <a href="https://www.freecodecamp.org/learn/responsive-web-design/#responsive-web-design-projects" target="_blank">Responsive Web Design</a> course on freeCodeCamp.
-                I was able to earn a <a href="https://www.freecodecamp.org/certification/gocs/responsive-web-design" target="_blank">Certification</a> for completing the following: </p> 
-                <div className="project-container">
-                    <div className="project-tile anchored large">
-                        <div className="project-detail">
-                            <h3 title="">Technical Documentation Page</h3>
-                            <p className="description">JavaScript technical documentation page</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://codepen.io/dennisgocong/full/VwyogOx" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://codepen.io/dennisgocong/pen/VwyogOx" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                    </div>
-
-                    <div className="project-tile anchored large">
-
-                        <div className="project-detail">
-                            <h3 title="">Personal Portfolio Webpage</h3>
-                            <p className="description">My first personal portfolio webpage</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://codepen.io/dennisgocong/full/xxYENXR" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://codepen.io/dennisgocong/pen/xxYENXR" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                        
-                    </div>
-
-                    <div className="project-tile anchored large">
-
-                        <div className="project-detail">
-                            <h3 title="">Product Landing Page Landing</h3>
-                            <p className="description">Created my own product landing page for Victoria's Secret</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                                <li>JavaScript</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://codepen.io/dennisgocong/full/BaJEKja" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://codepen.io/dennisgocong/pen/BaJEKja" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                
-                    </div>
-
-                    <div className="project-tile anchored large">
-                        <div className="project-detail">
-                            <h3 title="">Survey Form</h3>
-                            <p className="description">A fitness suite survey form</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://codepen.io/dennisgocong/full/xxXNKvK" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://codepen.io/dennisgocong/pen/xxXNKvK" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                    
-                    </div>
-
-                    <div className="project-tile anchored large">
-
-                        <div className="project-detail">
-                            <h3 title="">Tribute Page</h3>
-                            <p>A tribute page for Dr. Norman Borlaug</p>
-                            <ul className="tech-stack">
-                                <li>HTML</li>
-                                <li>CSS</li>
-                            </ul>
-                        </div>
-                        <div className="links">
-                            <a href="https://codepen.io/dennisgocong/full/NWaLmbB" className="demo-link" target="_blank">Demo <FaExternalLinkAlt className="link-icons"/></a>
-                            <a href="https://codepen.io/dennisgocong/pen/NWaLmbB" className="source-link" target="_blank">Source <FaExternalLinkAlt className="link-icons"/></a>
-                        </div>
-                    
-                    </div>
-                    
-                </div> 
-
-            </div>}
+                <div id="FreeCodeCamp-projects">
+                    <h2>FreeCodeCamp(Projects)</h2> 
+                    <p>This is my projects for <a href="https://www.freecodecamp.org/learn/responsive-web-design/#responsive-web-design-projects" target="_blank">Responsive Web Design</a> course on freeCodeCamp.
+                    I was able to earn a <a href="https://www.freecodecamp.org/certification/gocs/responsive-web-design" target="_blank">Certification</a> for completing the following: </p> 
+                    <div className="project-container">
+                        {moreProject.map((project)=> projectElements(project))}                    
+                    </div> 
+                </div>
+            }
             
         </section>
     );
