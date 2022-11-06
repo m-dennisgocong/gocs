e3ea7d56164ee931973d7eb19a4179846957ad5a
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-06 13:07:57 +0700

    feat: add new data ArtsList.js
    myArts array containes the href link and  image source for my arts

---
+++ src/data/ArtsList.js
@@ -0,0 +1,69 @@
+export const myArts = [
+    {
+        link: "https://codepen.io/dennisgocong/pen/dyJqzQX",
+        img : "/cat.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/zYpZXjB",
+        img : "/avocado.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/WNdEqpv",
+        img : "/twitter-logo.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/XWVadVK",
+        img : "/pixelated-sunglass.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/rNJXzze",
+        img : "/add-even-pattern.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/MWQxJKa",
+        img : "/glitch-effect.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/JjLjwEo",
+        img : "/pi.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/ExQGmEW",
+        img : "/counter.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/QWaggKG",
+        img : "/loading-animation.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/vYeQyEW",
+        img : "/batman-logo.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/jOZoKex",
+        img : "/walter-white-01.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/yLvWqPd",
+        img : "/walter-white-02.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/MWQMaaL",
+        img : "/walter-white-03.png"
+    },
+    {
+        link: "https://codepen.io/dennisgocong/pen/oNErbrJ",
+        img : "/walter-white-04.png"
+    }
+];
+
+
+
+
+
+
+
+
+
+
+
