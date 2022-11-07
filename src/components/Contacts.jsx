f7c26305e92b4d7486fba87ee5049924cf7d05a3
Dennis Goc-ong <86005147+m-dennisgocong@users.noreply.github.com>
2022-11-07 13:44:43 +0700

    style: break social logo link to different line

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -8,7 +8,11 @@ const Contacts = () => {
             <p>Get in touch for opportunities or just to say hi! 👋 <br /> <span>m.dennisgocong@gmail.com</span></p>
 
             <div id="socials-wrapper">
-                <a href="https://github.com/m-dennisgocong" target="_blank"><FaGithub/></a> <a href="https://twitter.com/dennisgocong" target="_blank"><FaTwitter/></a> <a href="https://www.freecodecamp.org/gocs" target="_blank"><FaFreeCodeCamp/></a> <a href="https://codepen.io/dennisgocong" target="_blank"><FaCodepen/></a> <a href="https://replit.com/@m-dennisgocong" target="_blank"><SiReplit/></a>
+                <a href="https://github.com/m-dennisgocong" target="_blank"><FaGithub/></a> 
+                <a href="https://twitter.com/dennisgocong" target="_blank"><FaTwitter/></a> 
+                <a href="https://www.freecodecamp.org/gocs" target="_blank"><FaFreeCodeCamp/></a> 
+                <a href="https://codepen.io/dennisgocong" target="_blank"><FaCodepen/></a> 
+                <a href="https://replit.com/@m-dennisgocong" target="_blank"><SiReplit/></a>
             </div>
         </section>
     );
